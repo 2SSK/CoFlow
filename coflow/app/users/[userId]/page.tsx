30348b09d054c29ee7e3aa9e73318336dde74d7b
S A U R A V <sauravchp2@gmail.com>
2024-02-04 11:57:57 +0530

    feat: Add dynamic user ID page in app/users/[userId] route

---
+++ coflow/app/users/[userId]/page.tsx
@@ -0,0 +1,9 @@
+interface UserIdPageProps {
+    params: {
+        userId: string;
+    };
+}
+
+const Page = ({ params }: UserIdPageProps) => {
+    return <div>User ID: {params.userId}</div>;
+};
