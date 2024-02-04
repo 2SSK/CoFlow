82927d0f4812cf425d765a90cb117abe1e9a7687
S A U R A V <sauravchp2@gmail.com>
2024-02-04 12:33:01 +0530

    refactor: Replace HTML button with custom Button component in Home page

--- coflow/app/page.tsx
+++ coflow/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
         <>
-            <button>Click me</button>
+            <Button>Click me</Button>
         </>
     );
 }
