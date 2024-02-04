69f32c48c050ca582c4915194574415a25bec2a0
S A U R A V <sauravchp2@gmail.com>
2024-02-04 17:55:35 +0530

    style: Format RootLayout component in layout.tsx
    Aligned indentation and formatted the RootLayout component in layout.tsx
    for better readability and consistency.
    
    - Adjusted indentation for title and description properties inthe
      metadata object.
    - Improved formatting for the RootLayout component for a more organized
      structure.

---
+++ coflow/providers/convex-client-provider.tsx
@@ -0,0 +1,33 @@
+// Importing necessary dependencies
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+import { ConvexClientProvider as LocalConvexClientProvider } from "./convex-client-provider";
+import { Children } from "react";
+
+// Defining the props for ConvexClientProvider component
+interface ConvexClientProviderProps {
+    children: React.ReactNode;
+}
+
+// Retrieving the Convex URL from environment variables
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+// Creating an instance of ConvexReactClient with the specified URL
+const convex = new ConvexReactClient(convexUrl);
+
+// Exporting the ConvexClientProvider component
+export const ConvexClientProvider = ({
+    children,
+}: ConvexClientProviderProps) => {
+    return (
+        // Wrapping the application with ClerkProvider and ConvexProviderWithClerk
+        <ClerkProvider>
+            <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+                {children}
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    );
+};
