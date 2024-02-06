e5977901e97c97b68ca057ae9e36070981597553
S A U R A V <sauravchp2@gmail.com>
2024-02-06 06:17:16 +0530

    feat(api): Integrated board module  into Convex API
    In this commit, I integrated the board module into the Convex API dby
    importing its types into the generated API declaration file. This
    enables seamless access to board-related functionalities within Convex,
    enhancing the overall functionality and flexibility of the API.
    
    ---
    
    feat(dataModle): incorporate Schema definition into Convex data model
    
    This commit incorporrates schema definitions into the Convex data model
    by utilizing the schema file to generate more accurate types for
    documents and IDs. By leveraging schema definitions, we enhance type
    safety and improve the overall reliability of the data model, enabling
    better development practices and reducing potential errors.

---
+++ coflow/convex/board.ts
@@ -0,0 +1,41 @@
+import { mutation } from "./_generated/server";
+import { v } from "convex/values";
+
+const images = [
+    "/placeholders/01.svg",
+    "/placeholders/02.svg",
+    "/placeholders/03.svg",
+    "/placeholders/04.svg",
+    "/placeholders/05.svg",
+    "/placeholders/06.svg",
+    "/placeholders/07.svg",
+    "/placeholders/08.svg",
+    "/placeholders/09.svg",
+    "/placeholders/10.svg",
+];
+
+export const create = mutation({
+    args: {
+        orgId: v.string(),
+        title: v.string(),
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
+
+        const randomImage = images[Math.floor(Math.random() * images.length)];
+
+        const board = await ctx.db.insert("boards", {
+            title: args.title,
+            orgId: args.orgId,
+            authorId: identity.subject,
+            authorName: identity.name!,
+            imageUrl: randomImage,
+        });
+
+        return board;
+    },
+});
